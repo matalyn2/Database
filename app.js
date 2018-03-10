43154e91aec790c404a375fb23f70028e28cf389
Matalyn Talbot <matalyn.nicole@gmail.com>
2018-03-10 16:13:30 -0700

    Finished uploads and data security

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
+  , fs = require('fs')
   , path = require('path');
 //var methodOverride = require('method-override');
 var app = express();
 var mysql      = require('mysql');
+const validator = require('express-validator');
 var bodyParser=require("body-parser");
+var busboy = require('connect-busboy');
 var connection = mysql.createConnection({
               host     : 'localhost',
               user     : 'matalynt',
@@ -28,8 +31,11 @@ app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use(validator());
+app.use(busboy());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.json({uploadDir:'/public/img/'}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'keyboard cat',
@@ -42,7 +48,7 @@ app.get('/', routes.index);//call for main index page
 app.get('/login', routes.index);//call for login page
 app.get('/home/logout', routes.index);//call for dashboard page after login
 
-app.get('/signup', user.signup);//call for signup page
+/*app.get('/signup', user.signup);//call for signup page*/
 app.get('/home/dashboard', user.dashboard);//call for dashboard page after login
 app.get('/home/kittens', user.kittens);//call for dashboard page after login
 app.get('/home/staff', user.staff);//call for dashboard page after login
@@ -54,7 +60,7 @@ app.get('/home/newstaff', user.newstaff);//call for dashboard page after login
 
 //Middleware
 app.post('/login', user.login);//call for login post
-app.post('/signup', user.signup);//call for signup post
+/*app.post('/signup', user.signup);//call for signup post*/
 app.post('/home/kittens', user.kittens);//call for dashboard page after login
 app.post('/home/staff', user.staff);//call for dashboard page after login
 app.post('/home/owners', user.owners);//call for dashboard page after login
--- routes/user.js
+++ routes/user.js
@@ -36,7 +36,7 @@ exports.login = function(req, res){
    }         
 };
 
-exports.signup = function(req, res){
+/*exports.signup = function(req, res){
    message = '';
    if(req.method == "POST"){
       var post  = req.body;
@@ -55,7 +55,7 @@ exports.signup = function(req, res){
    } else {
       res.render('signup');
    }
-};
+};*/
 
 exports.dashboard = function(req, res, next){
 	
@@ -67,8 +67,7 @@ exports.dashboard = function(req, res, next){
 		return;
 	}
 	 
-	 var sql="SELECT name, email FROM `user` WHERE `id`='"+userId+"'";
-	 
+	 var sql="SELECT name, email FROM `user` WHERE `id`='"+userId+"'";	 
 	   db.query(sql, function(err, results){
 		   
 		   console.log(results);
@@ -83,20 +82,32 @@ exports.kittens = function(req, res, next){
 	
 	var user =  req.session.user,
 	userId = req.session.userId;
+	var message;
 
    if(req.method == "POST"){
       var post  = req.body;
       var kitten_name= post.kitten_name;
+      if(!kitten_name)
+      {
+      	message = "Nothing was entered";
 
-      var sql="DELETE FROM kitten WHERE id='"+kitten_name+"'";	 
-	   	db.query(sql, function(err, results){
-		   console.log(results.affectedRows);	  		  
-		});
+      } else {
 
-		var sql_select="SELECT * FROM `kitten`";	 
+	      var sql="DELETE FROM kitten WHERE id='"+kitten_name+"'";	 
+		   	db.query(sql, function(err, results){
+			   console.log(results.affectedRows);
+			   if(err){
+			   		message = "Row could not be deleted";
+			   }	  		  
+			});
+		}
+
+		var sql_select="SELECT * FROM `kitten`";
+		console.log(message);	 
 	   	db.query(sql_select, function(err, results){
-		   res.render('kittens.ejs', {kittens:results});	  		  
+		   res.render('kittens.ejs', {kittens:results, message:message});	  		  
 		});
+	   
 
   	} else{
 
@@ -107,7 +118,6 @@ exports.kittens = function(req, res, next){
 		 
 		var sql="SELECT * FROM `kitten`";	 
 	   	db.query(sql, function(err, results){
-
 		   res.render('kittens.ejs', {kittens:results});	  
 		  
 		});
@@ -119,19 +129,25 @@ exports.staff = function(req, res, next){
 	
 	var user =  req.session.user,
 	userId = req.session.userId;
+	var message;
 
 	 if(req.method == "POST"){
 	      var post  = req.body;
 	      var staff_name= post.staff_name;
+
+	      if(!staff_name){
+	      	message = "Nothing was entered";
+	      } else {
 
 	      var sql="DELETE FROM staff WHERE id='"+staff_name+"'";	 
 		   	db.query(sql, function(err, results){
 			   console.log(results.affectedRows);	  		  
 			});
 
+		   }
 			var sql_select="SELECT * FROM `staff`";	 
 		   	db.query(sql_select, function(err, results){
-			   res.render('staff.ejs', {staff:results});	  		  
+			   res.render('staff.ejs', {staff:results, message:message});	  		  
 			});
 
   	} else{
@@ -155,19 +171,24 @@ exports.owners = function(req, res, next){
 	
 	var user =  req.session.user,
 	userId = req.session.userId;
+	var message;
 
 	if(req.method == "POST"){
       var post  = req.body;
       var owner_name= post.owner_name;
+      if(!owner_name){
+      	message = "Nothing was entered";
+      } else {
 
       var sql="DELETE FROM owner WHERE id='"+owner_name+"'";	 
 	   	db.query(sql, function(err, results){
 		   console.log(results.affectedRows);	  		  
 		});
 
+	   }
 		var sql_select="SELECT * FROM `owner`";	 
 	   	db.query(sql_select, function(err, results){
-		   res.render('owners.ejs', {owner:results});	  		  
+		   res.render('owners.ejs', {owner:results, message:message});	  		  
 		});
   	} else{
 	
@@ -191,6 +212,7 @@ exports.newstaff = function(req, res, next){
 	
 	var user =  req.session.user,
 	userId = req.session.userId;
+	var message;
 
 	if(req.method == "POST"){
 
@@ -199,15 +221,19 @@ exports.newstaff = function(req, res, next){
       var email = post.staff_email;
       var phone = post.staff_phone;
       var address = post.staff_address;
-
+      if(!name || !email || !phone || !address){
+      	message = "Missing information on form";
+      }
+      else {
       var sql = "INSERT INTO staff(name, email, phone, address) VALUES ('" + name + "','" + email + "','" + phone + "','" + address +"')";
 	   	db.query(sql, function(err, results){
 		   console.log(results);	  		  
 		});
+	   }
 
 		var sql_select="SELECT * FROM `owner`";	 
 	   	db.query(sql_select, function(err, results){
-		   res.render('staff.ejs', {staff:results});	  		  
+		   res.render('staff.ejs', {staff:results, message:message});	  		  
 		});
 
   	} else {
@@ -227,6 +253,7 @@ exports.newowner = function(req, res, next){
 	
 	var user =  req.session.user,
 	userId = req.session.userId;
+	var message;
 
 	if(req.method == "POST"){
       var post  = req.body;
@@ -234,15 +261,18 @@ exports.newowner = function(req, res, next){
       var email = post.owner_email;
       var phone = post.owner_phone;
       var address = post.owner_address;
+      if(!name || !email || !phone || !address){
+      	message = "Missing information on form";
+      } else {
 
       var sql = "INSERT INTO owner(name, email, phone, address) VALUES ('" + name + "','" + email + "','" + phone + "','" + address +"')";
 	   	db.query(sql, function(err, results){
 		   console.log(results);	  		  
 		});
-
+	   }
 		var sql_select="SELECT * FROM `owner`";	 
 	   	db.query(sql_select, function(err, results){
-		   res.render('owners.ejs', {owner:results});	  		  
+		   res.render('owners.ejs', {owner:results, message: message});	  		  
 		});
 
   	} else {
@@ -258,29 +288,68 @@ exports.newowner = function(req, res, next){
 	}	 
 };
 
+
+
 exports.newkitten = function(req, res, next){
-	
+	var path = require('path');
+	var fs = require('fs');
+	var busboy = require('connect-busboy');
+	var multiparty = require('multiparty');
+
 	var user =  req.session.user,
 	userId = req.session.userId;
 
 	if(req.method == "POST"){
+		var formData = new multiparty.Form();
+		var name;
+		var history;
+		var age;
+		var fertility;
+		var adopted;
+	    var photos = req.pipe(req.busboy);  
+      	var fstream;
+      	var message;
+      
+		formData.parse(req, function(err, fields, files) {
+	    	 //here you can read the appropriate fields/files
+	    	 let n = Object.values(files);
+	    	 let x = n[0];
+	    	 let y = x[0];
+	    	 name = fields.kitten_name;
+	    	 history = fields.kitten_history;
+	    	 age = fields.kitten_age;
+	    	 if(fields.kitten_fertility){ fertility = 1;} else{ fertility = 0;}
+			 if(fields.kitten_adopted){ adopted = 1;} else{ adopted = 0;}
+			var filename = y.originalFilename;
+			var targetPath = '/img/' + filename;
+			if(name !== '' || history !== '' || age !== '' || filename !== ''){
+				message = "Missing information on form."
+			} else {
+				var sql = "INSERT INTO kitten(name, history, age, fertility, adopted, photos) VALUES ('" + name + "','" + history + "'," + age + "," + fertility +"," + adopted + ",'" + targetPath + "')";
+			   	db.query(sql, function(err, results){
+				   console.log(results);
+				   console.log(err);	  		  
+				});
+				
+			}
 
-      var post  = req.body;
-      var name= post.kitten_name;
-      var history = post.kitten_history;
-      var age = post.kitten_age;
-      if(post.kitten_fertility){ var fertility = 1;} else{ var fertility = 0;}
-      if(post.kitten_adopted){ var adopted = 1;} else{ var adopted = 0;}
-      var photos = post.kitten_photos;
+   		});
 
-      var sql = "INSERT INTO kitten(name, history, age, fertility, adopted, photos) VALUES ('" + name + "','" + history + "'," + age + "," + fertility +"," + adopted + ",'" + photos + "')";
-	   	db.query(sql, function(err, results){
-		   console.log(results);	  		  
-		});
+   		req.busboy.on('file', function (fieldname, file, filename) {
+        	if(filename){
+		        fstream = fs.createWriteStream('./public/img/' + filename);
+		        file.pipe(fstream);
+		        fstream.on('close', function () {
+		            //res.redirect('back');
+		        });
+	    	}
 
+    	});
+
+   		console.log(message);
 		var sql_select="SELECT * FROM `kitten`";	 
 	   	db.query(sql_select, function(err, results){
-		   res.render('kittens.ejs', {kittens:results});	  		  
+		   res.render('kittens.ejs', {kittens:results, message:message});	  		  
 		});
 
   	} else {
